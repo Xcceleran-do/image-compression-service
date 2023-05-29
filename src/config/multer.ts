98056cdcbf7e664e462046486bf7f6b76242b694
dave <dwtmekonnen123@gmail.com>
2023-05-29 13:55:42 +0300

    create upload folder if it doesn't exist

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,8 +1,15 @@
 import multer from "multer";
 import crypto from "crypto";
+import fs from "fs";
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: (req, file, cb) => {
+    const dir = "uploads/";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
+  },
   filename: function (req, file, cb) {
     cb(null, crypto.randomBytes(20).toString("hex"));
   },
