ef3b3fcddf67bb04d990cfcb0e9f70eb93402da5
dave <dwtmekonnen123@gmail.com>
2023-05-30 15:09:46 +0300

    refactor routes

--- src/routes/compressImageRoutes.ts
+++ src/routes/compressImageRoutes.ts
@@ -3,11 +3,9 @@ import { handleImageCompression } from "../controllers/handleImageCompression";
 import { handleImageResize } from "../controllers/handleImageResize";
 import { handleImageConversion } from "../controllers/handleImageConversion";
 
-import { upload } from "../config/multer";
-
 const router = express.Router();
 
-router.post("/compress", upload.single("image"), handleImageCompression);
-router.post("/resize", upload.single("image"), handleImageResize);
-router.post("/convert", upload.single("image"), handleImageConversion);
+router.post("/compress", handleImageCompression);
+router.post("/resize", handleImageResize);
+router.post("/convert", handleImageConversion);
 export default router;
