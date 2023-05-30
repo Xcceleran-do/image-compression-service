89adde81f8e634644951bf23ac227353188c791c
dave <dwtmekonnen123@gmail.com>
2023-05-30 13:30:43 +0300

    update base api endpoint

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.json(data);
 });
 
-app.use("/api/v1/compress", compressionRouter);
+app.use("/api/v1/image", compressionRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
