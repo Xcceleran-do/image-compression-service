28dcd7f661e89f7215a9bbcc9d638f3db1b7d90e
dave <dwtmekonnen123@gmail.com>
2023-05-30 14:48:12 +0300

    updated route for compress

--- src/routes/compressImageRoutes.ts
+++ src/routes/compressImageRoutes.ts
@@ -7,7 +7,7 @@ import { upload } from "../config/multer";
 
 const router = express.Router();
 
-router.post("/", upload.single("image"), handleImageCompression);
+router.post("/compress", upload.single("image"), handleImageCompression);
 router.post("/resize", upload.single("image"), handleImageResize);
 router.post("/convert", upload.single("image"), handleImageConversion);
 export default router;
