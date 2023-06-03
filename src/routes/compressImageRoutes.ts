8bbb96e575580da4bbf0ff42885bdc146d9091ff
dave <dwtmekonnen123@gmail.com>
2023-06-03 15:03:06 +0300

    implement quality validation middleware

--- src/routes/compressImageRoutes.ts
+++ src/routes/compressImageRoutes.ts
@@ -3,9 +3,11 @@ import { handleImageCompression } from "../controllers/handleImageCompression";
 import { handleImageResize } from "../controllers/handleImageResize";
 import { handleImageConversion } from "../controllers/handleImageConversion";
 import { handleDefaultConfiguration } from "../controllers/handleDefaultConfiguration";
+import { validateQuality } from "../middleware/validate.quality.middleware";
+
 const router = express.Router();
 
-router.post("/compress", handleImageCompression);
+router.post("/compress", validateQuality, handleImageCompression);
 router.post("/resize", handleImageResize);
 router.post("/convert", handleImageConversion);
 router.post("/default", handleDefaultConfiguration);
